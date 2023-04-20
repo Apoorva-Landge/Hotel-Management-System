fce2a7f884edef50e09c0fc1252959a23a02f42a
dac <sumaiyyajamadar23@gmail.com>
2023-04-20 14:53:52 +0530

    Java File

---
+++ java files/Receptionist.java
@@ -0,0 +1,25 @@
+
+public class Receptionist {
+   private int rescepid;
+   private String rname;
+   
+public int getRescepid() {
+	return rescepid;
+}
+public void setRescepid(int rescepid) {
+	this.rescepid = rescepid;
+}
+public String getRname() {
+	return rname;
+}
+public void setRname(String rname) {
+	this.rname = rname;
+}
+public Receptionist(int rescepid, String rname) {
+	super();
+	this.rescepid = rescepid;
+	this.rname = rname;
+}
+   
+ 
+}
